e5246e6097f2f445f5609d8c5e121870f6e5a92a
Kabelo Rammala <starreputes@gmail.com>
2024-04-21 11:30:34 +0200

    add a get request to fetch restuarants

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 const port = 3000;
+
+// fetch restaurants data from api with a postcode
+app.get("/", async (req, res) => {
+  const postCode = req.query.postcode;
+
+  const api = `https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postCode}`;
+
+  try {
+    const response = await axios.get(api);
+    console.log(response);
+  } catch (error) {
+    console.log("Error", error);
+  }
+});
 
 // start the application by listening on a port
 app.listen(port, () => {
